9829565f44f9ec27f9e436f7b01928e077f67006
DaveSecOps <david@hwy9.ai>
2025-06-19 17:36:48 -0400

    docs: add Apache 2.0 license and update package metadata
    - Add LICENSE file with Apache 2.0 license
    - Update license classifiers in setup.py and pyproject.toml
    - Set copyright year to 2025 and add author name

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="drivetime",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="David Langlands",
+    author_email="david@langlands.com",
     description="Get live driving ETAs with traffic information",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,7 +26,8 @@ setup(
     python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
+    license="Apache-2.0",
 )
